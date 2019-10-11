3fb4764accd4128ee6b8d09b4f6df6f0cd6ed825
droidboi <54546961+droidboi@users.noreply.github.com>
2019-10-11 09:01:31 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+print("When entering the options, please enter them exactly as presented.")
+print("The world is ending")
+print("The year is 42069, and Artificial Intelligence has taken control of the world. It is your job to find survivors and supplies. Be careful, most automated or electronic devices may be controlled by the enemies.")
+print("You were across the country from your family studying mechanical engineering in college. When the first attack occured, you were flying back home.")
+print("Unfortunately, your plane got controlled by AI and crash-landed into a lake. It is your job to find supplies and reach your family.")
+print("You wake up in the plane and take off your seatbelt, seeing the destruction around you. You have 5 minutes before the plane sinks beneath the lake.")
+q = int(input("Enter '1' if you want to help other passengers or enter '2' if you want to ignore the cries to gather supplies and escape the plane instead. "))
+if q == 1:
+    print ("You barely mananaged to escape with 3 other people. One is a doctor, the second is another student, and the third is a profesional athlete.")
+elif q == 2:
+    print ("You managed to gather supplies and escape the plane. However, the supplies were not good.")
+if q == 2:
+    print("You see an abandoned warehouse")
+r = int(input("Enter '1' if you decide to go inside or enter '2' if you decide to stay with the supplies you have."))
+if r == 1:
+    print("As you enter the warehouse, you get cut on a rusty pipe you tried pulling out. Without proper care, you die of an infecte arm.")
+if r == 2:
+    print("Your supplies are useless and you die of starvation.")
+if q == 1:
+    print("You see an abandoned warehouse.")
+s = int(input("Enter '1' to enter the warehouse or enter '2' to stay."))
+if s == 1:
+    print("As you enter the warehouse, you cut yourself on a rusty pipe. Luckily, the doctor as medical equipment that helped 
+ 
