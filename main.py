66c8bf14b837ec61ab121c5c883874967c2b972e
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-10-11 15:59:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,20 @@ print("You wake up in the plane and take off your seatbelt, seeing the destructi
 q = int(input("Enter '1' if you want to help other passengers or enter '2' if you want to ignore the cries to gather supplies and escape the plane instead. "))
 if q == 1:
     print ("You barely mananaged to escape with 3 other people. One is a doctor, the second is another student, and the third is a profesional athlete.")
+    print("You see an abandoned warehouse.")
+    s = int(input("Enter '1' to enter the warehouse or enter '2' to stay."))
+    if s == 1:   
+        print("As you enter the warehouse, you cut yourself on a rusty pipe. Luckily, the doctor has medical equipment that healed you. As you explore the warehouse, you find food and weapons.")
+    elif s == 2: 
+        print("Eventually, you go delirious due to a lack of water and kill the doctor. Later, you get cut on a twig and die without proper medical attention.")
 elif q == 2:
     print ("You managed to gather supplies and escape the plane. However, the supplies were not good.")
-if q == 2:
     print("You see an abandoned warehouse")
-r = int(input("Enter '1' if you decide to go inside or enter '2' if you decide to stay with the supplies you have."))
-if r == 1:
-    print("As you enter the warehouse, you get cut on a rusty pipe you tried pulling out. Without proper care, you die of an infecte arm.")
-if r == 2:
-    print("Your supplies are useless and you die of starvation.")
-if q == 1:
-    print("You see an abandoned warehouse.")
-s = int(input("Enter '1' to enter the warehouse or enter '2' to stay."))
-if s == 1:
-    print("As you enter the warehouse, you cut yourself on a rusty pipe. Luckily, the doctor as medical equipment that helped 
+    r = int(input("Enter '1' if you decide to go inside or enter '2' if you decide to stay with the supplies you have."))
+    if r == 1:
+        print("As you enter the warehouse, you get cut on a rusty pipe you tried pulling out. Without proper care, you die of an infected arm.")
+    if r == 2:
+        print("Your supplies are useless and you die of starvation.")
+s = input("With your newly found supplies you are able to make it to a new town. As you search through the town, y
+
  
